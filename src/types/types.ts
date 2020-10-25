14e8f2e5e9bb20946775e38f7ac01562ea305a80
mhsss <tostau123@gamil.com>
2020-10-25 23:03:11 +0200

    all
    all

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type formDataType = {
+    title: string
+    descr: string
+    file: File
+}
+export type ItemPropsType = {
+    img: string
+    title: string
+    descr: string
+    name: string
+    size: number
+    type: string
+    width: number
+    height: number
+}
